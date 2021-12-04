a74ed2d35f8c312a2f9c4b196651b8e07830ca02
idegtyarev <cum_butt@mail.ru>
2021-12-04 21:39:23 +0300

    initial commit

---
+++ ridt_site/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RidtSiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ridt_site'
---
+++ ridt_site/views.py
@@ -0,0 +1,29 @@
+from PIL import Image
+from PIL.JpegImagePlugin import JpegImageFile
+from django.core.files.base import ContentFile
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+
+# Create your views here.
+
+def RidtMain(request):
+    return render(request, 'main/home.html')
+
+
+def UrlInput(request,):
+    url = request.POST.get('url', '')
+    return render(request, 'main/constructor.html', context={'url': url})
+
+
+def Constructor(request,):
+    url = request.POST.get('url', '')
+    app_name = request.POST.get('app_name', '')
+    app_id = request.POST.get('app_id', '')
+    image = request.FILES['image']
+
+    image: JpegImageFile = Image.open(image)
+    image.save(open('cat.jpeg', 'wb'))
+    #open('cat.jpeg', 'wb').write(image)
+
+    return render(request, 'main/rate_choise.html')
